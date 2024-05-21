c681656e62ced95b9e93baee52c4a9adfbf9f561
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-05-21 18:06:25 +0300

    conditional statements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python3
 
+
 def admin_login(username, password):
+    password = '9999'
+    username == 'Felooh'
+    if username == 'Felooh' and  password == '9999':
+        print(admin_login("Access granted"))
+    else:
+        print("Access denided")
+
+print(admin_login("oyaa", "1234"))
     # your code here
-    pass
+    
+    
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        return ("It's brisk out there!")
+    elif temperature > 40 and temperature < 65:
+        return ("It's a little chilly out there!")
+    elif temperature > 85:
+        return ("It's too dang hot out there!")
+    else:
+        return ("It's perfect out there!")
+print(hows_the_weather(88))
 
+
+## I beleive I am wrong
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num == 1:
+        return "Fizz"
+    elif num == 5:
+        return "Buzz"
+    elif num == 3 or num ==5:
+        return "FizzBuzz"
+    else:
+        return num
+    
+print(fizzbuzz(23))
+
+     # your code here
+    
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        return num1 / num2
+    else:
+        return "Invalid operator."
+print(calculator("*", 2, 3))
